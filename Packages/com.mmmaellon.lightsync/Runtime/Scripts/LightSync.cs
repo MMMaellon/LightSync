a6c265f41f217437de5b90c0106db61233fe8abf
cameronmcnz <mmmaellon@gmail.com>
2024-05-17 01:56:58 -0700

    boilerplate for the data object

---
+++ Packages/com.mmmaellon.lightsync/Runtime/Scripts/LightSync.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace MMMaellon.LightSync
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LightSync : UdonSharpBehaviour
+    {
+        public LightSyncData data;//Gets created in the editor, as an invisible child of this object. Because it's a separate object we don't get issues with ownership and dumb stuff like that.
+
+        public void OnSendData()
+        {
+
+        }
+
+        public void OnReceiveData(VRC.Udon.Common.DeserializationResult result)
+        {
+
+        }
+    }
+}
